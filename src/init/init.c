77f0c760cf337c8e66b09c47a4c9c5854b66648e
LilBoooopp <cbopp@student.42lausanne.ch>
2025-01-26 20:53:55 +0000

    Prints current user in loop

--- src/init/init.c
+++ src/init/init.c
@@ -1,6 +1,18 @@
 #include "../../include/minishell.h"
 
-int	setupenv(t_mini mini)
+void	getuser(t_mini *mini)
 {
-	
+	int	i;
+
+	i = 0;
+	while (mini->envp[i] && ft_strnstr(mini->envp[i], "USER", 4) == 0)
+	{
+		(mini->envp)++;
+		i++;
+	}
+	mini->user = ft_strdup(mini->envp[i] + 5);
+}
+void	setupenv(t_mini *mini)
+{
+	getuser(mini);
 }
