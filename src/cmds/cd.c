3d35b38e2120309391d0ada26f81d2cb74cc29b5
LilBoooopp <cbopp@student.42lausanne.ch>
2025-01-27 15:35:19 +0000

    functional cd without absolute path

--- src/cmds/cd.c
+++ src/cmds/cd.c
@@ -1,1 +1,15 @@
 #include "../../include/minishell.h"
+
+/**
+ * @brief Basic cd, not completed
+ * @param constchar *path
+ * @warning DOES NOT WORK WITH ABSOLUTE PATH
+ */
+int	cd(const char *path)
+{
+	if (chdir(path) == 0)
+		return (0);
+	perror("cd");
+	(void)path;
+	return (1);
+}
