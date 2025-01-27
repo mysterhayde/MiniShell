8015770addcaef4d271c646597536596a54498d1
LilBoooopp <cbopp@student.42lausanne.ch>
2025-01-27 19:44:35 +0000

    ft_realloc doc

--- libft/Libft/ft_realloc.c
+++ libft/Libft/ft_realloc.c
@@ -3,7 +3,7 @@
 /**
  * @brief realloc
  * @param void *ptr you want to realloc
- * @param size_t size of the new ptr
+ * @param size_t "sizeof(vartype) * size" to specify what type of size it needs to malloc
  */
 void	*ft_realloc(char *ptr, size_t size)
 {
