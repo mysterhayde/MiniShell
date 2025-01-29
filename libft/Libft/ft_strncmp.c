3845d85a4549c07a92ffc4433c7bc1e6784473d6
LilBoooopp <cbopp@student.42lausanne.ch>
2025-01-29 17:07:52 +0000

    fixed strncmp into strmincmp

--- libft/Libft/ft_strncmp.c
+++ libft/Libft/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cbopp <cbopp@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 10:54:33 by hdougoud          #+#    #+#             */
-/*   Updated: 2024/11/27 10:56:29 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/01/29 15:13:19 by cbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,30 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	us1 = (unsigned char *) s1;
 	us2 = (unsigned char *) s2;
 	while (n > i)
+	{
+		if (us1[i] == '\0' && us2[i] == '\0')
+			return (0);
+		if (us1[i] != us2[i] || us1[i] == '\0' || us2[i] == '\0')
+			return (us1[i] - us2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_strmincmp(const char *s1, const char *s2, size_t min)
+{
+	unsigned int	i;
+	unsigned char	*us1;
+	unsigned char	*us2;
+	size_t			size;
+
+	i = 0;
+	us1 = (unsigned char *) s1;
+	us2 = (unsigned char *) s2;
+	size = ft_strlen(s1);
+	if (size < min)
+		size = min;
+	while (size > i)
 	{
 		if (us1[i] == '\0' && us2[i] == '\0')
 			return (0);
