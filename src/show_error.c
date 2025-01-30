38a0ada2648353ddb7b432c785618db6577d154a
Hayden Dougoud <hdougoud@c1r11s4.42lausanne.ch>
2025-01-30 17:10:32 +0100

    start parsing (test)

---
+++ src/show_error.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   show_error.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 15:20:05 by hdougoud          #+#    #+#             */
+/*   Updated: 2025/01/30 16:10:00 by hdougoud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+void	show_error(char *str)
+{
+	ft_putendl_fd(COLOR_RED"Error", 2);
+	perror(str);
+	ft_putendl_fd(COLOR_RESET, 2);
+}
