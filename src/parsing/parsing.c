593dc9f803677822fdcbd93e5dcf6050e38f81e3
mysterhayde <43919667+mysterhayde@users.noreply.github.com>
2025-01-31 18:48:14 +0100

    exec commandes

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,17 +6,39 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 10:00:47 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/01/30 16:11:56 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/01/31 18:20:47 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+
 char	*parsing(char *str)
 {
-	ft_split(str, ' ');
-	if (str == NULL)
-		show_error("Parsing failed");
-	printf("Parsed %s\n", str);
+	int		i;
+	char **tab;
+	pid_t	pid;
+	char	*bin;
+
+	i = 0;
+	tab = ft_split(str, ' ');
+	while (tab[i])
+	{
+		tab[i] = ft_strtrim(tab[i], " ");
+		i++;
+	}
+	i = 0;
+	bin = ft_strjoin("/bin/", tab[0]);
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(bin, tab, NULL) == -1)
+			show_error("commande not found");
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+	}
+	free(bin);
 	return (str);
 }
--- src/show_error.c
+++ src/show_error.c
@@ -6,11 +6,11 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 15:20:05 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/01/30 16:10:00 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/01/31 17:54:39 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/minishell.h"
+#include "../include/minishell.h"
 
 void	show_error(char *str)
 {
