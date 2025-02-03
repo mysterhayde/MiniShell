7b7b7c440dee743651f6682cfdc737b5efe024fc
LilBoooopp <cbopp@student.42lausanne.ch>
2025-02-03 13:36:29 +0000

    builtin commands not working

--- src/cmds/echo.c
+++ src/cmds/echo.c
@@ -5,16 +5,17 @@
  * @param t_bool newline - TRUE=print FALSE=!print
  * @param char *str - String to print
  * @returns 1 on success 0 on fail
+ * @warning HAS TO BE FIXED
  */
-int	echo(t_bool newline, char *str)
+int	echo(char **cmd)
 {
 	int	ret;
 
-	ret = 0;
-	if (newline)
-		ret += ft_printf("%s", str);
-	ret += ft_printf("\n");
-	if (ret == 0)
-		return (1);
+	// ret = 0;
+	// if (newline)
+	// 	ret += ft_printf("%s", str);
+	// ret += ft_printf("\n");
+	// if (ret == 0)
+	// 	return (1);
 	return (0);
 }
---
+++ src/cmds/export.c
@@ -0,0 +1,8 @@
+#include "../../include/minishell.h"
+
+int	export(t_mini mini)
+{
+	char	**tempenv;
+
+	
+}
