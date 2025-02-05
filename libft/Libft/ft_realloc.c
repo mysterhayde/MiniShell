7caefca84f6ff8b339492b0a359aa761acc2081b
LilBoooopp <cbopp@student.42lausanne.ch>
2025-02-05 18:18:25 +0000

    realloc header

--- libft/Libft/ft_realloc.c
+++ libft/Libft/ft_realloc.c
@@ -1,9 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbopp <cbopp@student.42lausanne.ch>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 18:12:17 by cbopp             #+#    #+#             */
+/*   Updated: 2025/02/05 18:12:57 by cbopp            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft.h"
 
 /**
  * @brief realloc
  * @param void *ptr you want to realloc
- * @param size_t "sizeof(vartype) * size" to specify what type of size it needs to malloc
+ * @param size_t "sizeof(vartype) * size" to specify what 
+ * 				type of size it needs to malloc
  */
 void	*ft_realloc(void *ptr, size_t size)
 {
