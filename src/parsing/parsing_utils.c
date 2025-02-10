3958e0d0fac686cb368a7dfd849ee9cf5f7a6cd9
Hayden Dougoud <hdougoud@c1r9s4.42lausanne.ch>
2025-02-10 14:43:26 +0100

    pasring with signle quote

--- src/parsing/parsing_utils.c
+++ src/parsing/parsing_utils.c
@@ -1,39 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   count_words.c                                      :+:      :+:    :+:   */
+/*   parsing_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/08 15:28:50 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/02/10 14:20:08 by hdougoud         ###   ########.fr       */
+/*   Created: 2025/02/10 14:24:41 by hdougoud          #+#    #+#             */
+/*   Updated: 2025/02/10 14:33:56 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	count_words(char const *str)
+int	is_printable(char c)
 {
-	unsigned int	i;
-	unsigned int	word;
-
-	word = 0;
-	i = 0;
-
-	while(str[i])
-	{
-		if (str[i] == ' ')
-			i++;
-		while (str[i] && str[i] != ' ')
-		{
-			if (str[i] == 39)
-				while(str[++i] != 39 && str[i])
-					;
-			if (str[i] != ' ')
-				i++;
-		}
-		word++;	
-	}
-
-	return (word);
+	if(c == '!' || c == '$' || c == 39 || c == 34)
+		return (0);
+	else
+		return (1);
 }
--- src/parsing/split_entry.c
+++ src/parsing/split_entry.c
@@ -6,13 +6,13 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 11:51:45 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/02/10 11:33:33 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/02/10 14:40:49 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static unsigned int next_word_len(char *str)
+static unsigned int	next_word_len(char *str)
 {
 	int	i;
 	int	word_len;
@@ -21,42 +21,47 @@ static unsigned int next_word_len(char *str)
 	word_len = 0;
 	while (str[i] && str[i] != ' ')
 	{
-		if (str[i] == 34)
+		if (str[i] == 39)
 		{
-			while(str[++i != 34] && str[i])
+			while (str[++i] != 39 && str[i])
+					word_len++;
+			if (str[i + 1] == ' ')
+			{
 				word_len++;
+				break ;
+			}
 		}
-		word_len++;
 		i++;
+		word_len++;
 	}
-	//printf("Word_len %d\n", word_len);		//debug
-	return(word_len);
+	printf ("Word_len %d\n", word_len);		//debug
+	return (word_len);
 }
 
-static char **separate_args(char **split, char *str, int word)
+static char	**separate_args(char **split, char *str, int word)
 {
 	int	i;
 	int	j;
-	int word_len;
+	int	word_len;
 
-	j = 0;
-	while(word > 0)
+	j = -1;
+	while (word > 0)
 	{
 		i = 0;
 		str = ft_strtrim(str, " ");
 		word_len = next_word_len(str);
-		split[j] = malloc(sizeof(char) * (word_len + 1));
+		split[++j] = malloc(sizeof(char) * (word_len + 1));
 		if (!split[j])
-			return(NULL);
-		while(word_len > 0)
+			return (NULL);
+		while (word_len > 0)
 		{
+			if (*str == 39)
+				str++;
 			split[j][i++] = *str++;
 			word_len--;
 		}
 		split[j][i] = '\0';
-		//printf("str		%s\n", str);		//debug
-		printf("tab[j]	%s\n", split[j]);	//debug
-		j++;
+		printf("%s\n", split[j]);
 		word--;
 	}
 	split[j] = NULL;
@@ -69,10 +74,10 @@ char	**split_args(char *str)
 	char	**split;
 
 	word = count_words(str);
-	printf("Word count	%d\n", word);		//debug
+	printf ("Word count	%d\n", word);		//debug
 	split = malloc(sizeof(char *) * (word + 1));
 	if (!split)
-		return NULL;
+		return (NULL);
 	split = separate_args(split, str, word);
 	return (split);
 }
