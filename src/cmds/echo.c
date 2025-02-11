02537ef70ff346fdbcc8366a0b6f2f813c6be9dd
LilBoooopp <cbopp@student.42lausanne.ch>
2025-02-11 12:39:24 +0000

    echo -n working

--- src/cmds/echo.c
+++ src/cmds/echo.c
@@ -6,28 +6,53 @@
 /*   By: cbopp <cbopp@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:03:36 by cbopp             #+#    #+#             */
-/*   Updated: 2025/02/05 15:03:38 by cbopp            ###   ########.fr       */
+/*   Updated: 2025/02/11 12:37:11 by cbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+static t_bool	is_n_flag(char *str)
+{
+	int	i;
+
+	if (!str || str[0] != '-')
+		return (0);
+	i = 1;
+	while (str[i])
+	{
+		if (str[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (i > 1);
+}
+
 /**
- * @brief Writes str to std output with the option to 
- * @param t_bool newline - TRUE=print FALSE=!print
- * @param char *str - String to print
+ * @brief Writes str to std output with the -n option
+ * @param char **cmd
  * @returns 1 on success 0 on fail
- * @warning HAS TO BE FIXED
  */
 int	echo(char **cmd)
 {
-	// int	ret;
-	(void)cmd;
-	// ret = 0;
-	// if (newline)
-	// 	ret += ft_printf("%s", str);
-	// ret += ft_printf("\n");
-	// if (ret == 0)
-	// 	return (1);
+	int		i;
+	t_bool	n_flag;
+
+	n_flag = FALSE;
+	i = 1;
+	while (cmd[i] && is_n_flag(cmd[i]))
+	{
+		n_flag = TRUE;
+		i++;
+	}
+	while (cmd[i])
+	{
+		ft_printf("%s", cmd[i]);
+		if (cmd[i + 1])
+			ft_printf(" ");
+		i++;
+	}
+	if (!n_flag)
+		ft_printf("\n");
 	return (0);
 }
