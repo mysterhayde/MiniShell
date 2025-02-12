b4df1151caebc6ffed6bc2c046cd9c903be20387
Hayden <hayden@MacBookPro.localdomain>
2025-02-12 19:32:02 +0100

    quote management

--- src/parsing/parsing_utils.c
+++ src/parsing/parsing_utils.c
@@ -6,16 +6,42 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 14:24:41 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/02/10 14:33:56 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/02/12 18:11:03 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+int	is_operator(t_mini *mini, char *str)
+{
+	if (ft_strmincmp(str, "|", 1) == 0)
+	{
+		mini->is_pipe = TRUE;
+		return (1);
+	}
+	else if (ft_strmincmp(str, "<", 2) == 0)
+		return (2);
+	else if (ft_strmincmp(str, ">", 2) == 0)
+		return (2);
+	else if (ft_strmincmp(str, "<<", 1) == 0)
+		return (2);
+	else if (ft_strmincmp(str, ">>", 1) == 0)
+		return (2);
+	return (0);
+}
+
+int	is_separator(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n')
+		return (1);
+	else
+		return (0);
+}
+
 int	is_printable(char c)
 {
-	if(c == '!' || c == '$' || c == 39 || c == 34)
-		return (0);
-	else
+	if (c == '!' || c == '$' || c == '\"' || c == '\'')
 		return (1);
+	else
+		return (0);
 }
