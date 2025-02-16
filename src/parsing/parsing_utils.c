8da6eb1043f29766e2ace933d49615db6b1d1d65
mysterhayde <43919667+mysterhayde@users.noreply.github.com>
2025-02-16 16:05:46 +0100

    optimisation allocate_tokens function

--- src/parsing/parsing_utils.c
+++ src/parsing/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 14:24:41 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/02/16 14:53:11 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/02/16 15:55:54 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ int	is_operator(t_mini *mini, char *str)
 	if (ft_strmincmp(str, "|", 1) == 0)
 	{
 		mini->is_pipe = TRUE;
-		return (1);
+		return (PIPE);
 	}
-	else if (ft_strmincmp(str, "<", 2) == 0)
-		return (2);
-	else if (ft_strmincmp(str, ">", 2) == 0)
-		return (2);
-	else if (ft_strmincmp(str, ">>", 1) == 0)
-		return (2);
-	else if (ft_strmincmp(str, "<<", 1) == 0)
-		return (3);
+	else if (ft_strmincmp(str, "<", 1) == 0)
+		return (RDIT);
+	else if (ft_strmincmp(str, ">", 1) == 0)
+		return (RDIT);
+	else if (ft_strmincmp(str, ">>", 2) == 0)
+		return (RDIT);
+	else if (ft_strmincmp(str, "<<", 2) == 0)
+		return (HERE_DOC);
 	return (0);
 }
 
