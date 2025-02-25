ccd997af8c30c3bee03df328cd4e85d947ac7738
Cbopp Mancave <cbopp@student.42lausanne.ch>
2025-02-25 18:30:31 +0000

    Redirection works with pipes

--- src/exec/pipe.c
+++ src/exec/pipe.c
@@ -6,15 +6,60 @@
 /*   By: cbopp <cbopp@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 13:48:12 by cbopp             #+#    #+#             */
-/*   Updated: 2025/02/21 08:55:27 by cbopp            ###   ########.fr       */
+/*   Updated: 2025/02/25 18:22:32 by cbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+/**
+ * @brief Finds the next pipe token from the current position
+ * @param token starting token
+ * @return next pipe token or NULL if none found
+ */
+static t_token	*find_next_pipe(t_token *token)
+{
+	t_token	*current;
+
+	current = token;
+	while (current)
+	{
+		if (current->type == PIPE)
+			return (current);
+		current = current->next;
+	}
+	return (NULL);
+}
+
+/**
+ * @brief Applies redirectiojns up to the next pipe
+ * @param token starting token
+ */
+static void	apply_pipe_redir(t_token *token)
+{
+	t_token	*current;
+	t_token	*next_pipe;
+
+	current = token;
+	next_pipe = find_next_pipe(token);
+	while (current && current != next_pipe)
+	{
+		if (current->type == RDIT && current->next
+			&& current->next->type == FILES)
+			process_single_redir(current);
+		current = current->next;
+	}
+}
+
+/**
+ * @brief Sets up pipe redirections and applies file redicrections for a command
+ * @param i current command index
+ * @param mini shell state
+ * @param pipe_fds array of pipe file descriptors
+ */
 static void	handle_pipe_child(int i, t_mini *mini, int *pipe_fds)
 {
-	int	j;
+	int		j;
 
 	j = 0;
 	if (i > 0)
@@ -29,8 +74,16 @@ static void	handle_pipe_child(int i, t_mini *mini, int *pipe_fds)
 	}
 	while (j < (mini->pipe_num * 2))
 		close(pipe_fds[j++]);
+	apply_pipe_redir(mini->token);
 }
 
+/**
+ * @brief Executes a command in a pipeline
+ * @param mini shell state
+ * @param i current command index
+ * @param pipe_fds array of pipe file descriptors
+ * @return process ID on success, -1 on failure
+ */
 int	exec_pipe_cmd(t_mini *mini, int i, int *pipe_fds)
 {
 	pid_t	pid;
@@ -45,8 +98,6 @@ int	exec_pipe_cmd(t_mini *mini, int i, int *pipe_fds)
 		handle_pipe_child(i, mini, pipe_fds);
 		if (is_builtin(mini->token->cmd[0]))
 			exit(exec_builtin(mini, mini->token->cmd));
-		else if (is_builtin(mini->token->cmd[0]))
-			exit(exec_builtin(mini, mini->token->cmd));
 		else
 			exec_bin(mini, mini->token->cmd);
 		exit(1);
@@ -58,6 +109,11 @@ int	exec_pipe_cmd(t_mini *mini, int i, int *pipe_fds)
 	return (pid);
 }
 
+/**
+ * @brief Executes a pipeline of commands
+ * @param mini shell state
+ * @return exit status of the last command
+ */
 int	minipipe(t_mini *mini)
 {
 	t_pipe	p;
