e058a78ef799b45675bcabe5ff28964343f33d1a
mysterhayde <43919667+mysterhayde@users.noreply.github.com>
2025-02-28 16:03:08 +0100

    add expand_string correctly

--- src/parsing/clear_string.c
+++ src/parsing/clear_string.c
@@ -6,35 +6,56 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 16:40:34 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/02/28 12:49:48 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/02/28 16:01:45 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	check_string(t_mini *mini, t_token *cmd_token)
+void	transform_string(t_mini *mini, t_token *cmd_token)
 {
 	int	i;
 	int	j;
-	int	ret;
 
 	j = 0;
 	while (cmd_token->cmd[j])
 	{
 		i = 0;
-		while (cmd_token->cmd[j][i])
+		while (cmd_token->cmd[j][i]) //USE string search
+		{
+			if (cmd_token->cmd[j][i] == '\'')
+			{
+				while(cmd_token->cmd[j][++i] && cmd_token->cmd[j][i] != '\'')
+					;
+				i++;
+			}
 			if (cmd_token->cmd[j][i++] == '$')
+			{
 				cmd_token->cmd[j] = expand_string(cmd_token->cmd[j], mini->envp);
-			if (!cmd_token->cmd[j])
-				exit(EXIT_FAILURE); //TODO : free all and return prompt
+				if (!cmd_token->cmd[j])
+					exit(EXIT_FAILURE); //TODO : free all and return prompt
+			}
+		}
 		i = 0;
 		while (cmd_token->cmd[j][i])
-			if (cmd_token->cmd[j][i++] == '\"' || cmd_token->cmd[j][i++] == '\'')
+		{
+			if (cmd_token->cmd[j][i] == '\"' || cmd_token->cmd[j][i] == '\'')
+			{
 				cmd_token->cmd[j] = clean_quote(cmd_token->cmd[j]);
-			if (!cmd_token->cmd[j])
-				exit(EXIT_FAILURE); //TODO : free all and return prompt
+				if (!cmd_token->cmd[j])
+					exit(EXIT_FAILURE); //TODO : free all and return prompt
+			}
+			i++;
+		}
 		j++;
 	}
+}
+
+int	check_string(t_mini *mini, t_token *cmd_token)
+{
+	int	ret;
+
+	transform_string(mini, cmd_token);
 	if (is_builtin(cmd_token->cmd[0]))
 		ret = exec_builtin(mini, cmd_token->cmd);
 	else
