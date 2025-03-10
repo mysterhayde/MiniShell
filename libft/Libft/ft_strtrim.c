b41d3eb8d1c29684a6dc47aaad98e8fddb4f1352
Hayden Dougoud <hdougoud@c1r9s4.42lausanne.ch>
2025-03-10 14:56:08 +0100

    add ft_strtrim_free function

--- libft/Libft/ft_strtrim.c
+++ libft/Libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:31:40 by hdougoud          #+#    #+#             */
-/*   Updated: 2024/11/27 10:56:37 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/03/10 14:40:22 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,33 @@ char	*ft_strtrim(char const *s1, char const *set)
 	}
 	str[i] = '\0';
 	return (str);
+}
+
+char	*ft_strtrim_free(char *s1, char const *set)
+{
+	char	*s1_cpy;
+	char	*str;
+	int		end;
+	int		i;
+
+	s1_cpy = s1;
+	i = 0;
+	end = 0;
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	end = (ft_strlen(s1));
+	while (end >= 0 && ft_strchr(set, s1[end]))
+		end--;
+	str = malloc(sizeof(char) * (end + 2));
+	if (str == 0)
+		return (NULL);
+	while (i <= end)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (free(s1_cpy), str);
 }
 
 //int main(void)
