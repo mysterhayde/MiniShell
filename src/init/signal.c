967d1eaa4dcb8cd118eca4f69a6cda41269f6058
Hayden <mysterhayde@gmail.com>
2025-03-12 12:52:06 +0100

    signals seem to work

--- src/init/signal.c
+++ src/init/signal.c
@@ -6,7 +6,7 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 13:43:01 by cbopp             #+#    #+#             */
-/*   Updated: 2025/03/11 11:41:49 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/03/12 12:38:17 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,34 @@ int	g_signo;
 static void	handle_signal()
 {
 	write(1, "\n", 1);
-	rl_replace_line("", 0);
 	rl_on_new_line();
+	rl_replace_line("", 0);
 	rl_redisplay();
 }
 int	setup_signal_handlers()
 {
-	struct sigaction	sa;
-	
+	struct sigaction sa;
+
 	sa.sa_handler = handle_signal;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	if (sigaction(SIGINT, &sa, NULL))
-		return (0);
-	if (sigaction(SIGQUIT, &sa, NULL))
-		return (0);
-	return (1);
+	sa.sa_flags = SA_RESTART;
+	
+	struct sigaction sa_ignore;
+	sa_ignore.sa_handler = SIG_IGN;
+	sigemptyset(&sa_ignore.sa_mask);
+	sa_ignore.sa_flags = 0;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		return 0;
+	if (sigaction(SIGQUIT, &sa_ignore, NULL) == -1)
+		return 0;
+	return 1;
 }
 
-void	reset_signals_for_child(void)
-{
-	signal(SIGINT, SIG_DFL);
-	signal(SIGQUIT, SIG_DFL);
-}
+// void	reset_signals_for_child(void)
+// {
+// 	signal(SIGINT, SIG_DFL);
+// 	signal(SIGQUIT, SIG_DFL);
+// }
 
 // int	check_signal_interrupt(void)
 // {
