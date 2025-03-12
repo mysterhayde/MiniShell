0ea396b0d3e10c7cb0ef684480a8d000769d0ce1
Hayden <mysterhayde@gmail.com>
2025-03-12 15:46:05 +0100

    repair parsing with parenthese and nomred some files

--- src/init/signal.c
+++ src/init/signal.c
@@ -6,7 +6,7 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 13:43:01 by cbopp             #+#    #+#             */
-/*   Updated: 2025/03/12 14:33:35 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/03/12 15:43:35 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,31 @@
 #include <unistd.h>
 #include "../../include/minishell.h"
 
-int	g_signo;
-
-static void	handle_signal()
+static void	handle_signal(int signal)
 {
-	write(1, "\n", 1);
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
+	if (signal == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
-int	setup_signal_handlers()
+
+int	setup_signal_handlers(void)
 {
-	struct sigaction sa;
+	struct sigaction	sa;
+	struct sigaction	sa_ignore;
 
 	sa.sa_handler = handle_signal;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
-	
-	struct sigaction sa_ignore;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		return (1);
 	sa_ignore.sa_handler = SIG_IGN;
 	sigemptyset(&sa_ignore.sa_mask);
 	sa_ignore.sa_flags = 0;
-	if (sigaction(SIGINT, &sa, NULL) == -1)
-		return 1;
 	if (sigaction(SIGQUIT, &sa_ignore, NULL) == -1)
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
-
