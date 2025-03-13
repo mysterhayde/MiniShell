0432d37bced0214c6765418f7e72d4d4f76be4e0
mysterhayde <43919667+mysterhayde@users.noreply.github.com>
2025-03-13 10:55:51 +0100

    wildcard system test

--- src/before_exec/wildcard.c
+++ src/before_exec/wildcard.c
@@ -6,7 +6,7 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 16:46:21 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/03/12 17:07:06 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/03/13 10:20:59 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,11 @@ opendir()
 readdir()
 closedir()
 
-void wildcard()
+void wildcard(char *pwd)
+{
+	struct DIR	dir;
+
+	if (dir *opendir(str) == NULL)
+		return ;
+	
+}
