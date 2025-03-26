f94b57fa4160e35d199709a58177062408f3c9bc
Hayden <mysterhayde@gmail.com>
2025-03-26 17:16:02 +0100

    remove quote in file name

--- src/before_exec/check_string.c
+++ src/before_exec/check_string.c
@@ -6,7 +6,7 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 16:40:34 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/03/26 15:38:32 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/03/26 17:11:04 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,7 +99,7 @@ int	transform_string(t_token *current, char **envp, int return_code)
 		if (error || !current->cmd[j])
 			return (handle_var_error(current->cmd[0]));
 		current->cmd[j] = search_error_code(return_code, current->cmd[j]);
-		if (strchr(current->cmd[j], '\"') || strchr(current->cmd[j], '\''))
+		if (ft_strchr(current->cmd[j], '\"') || ft_strchr(current->cmd[j], '\''))
 		{
 			current->cmd[j] = clean_quote(current->cmd[j]);
 			if (!current->cmd[j])
