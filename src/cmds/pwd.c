68f26c38a0204203c808fe6f4ff62ef7f93376c6
Cbopp Mancave <cbopp@student.42lausanne.ch>
2025-03-27 22:53:15 +0000

    fixed pwd

--- src/cmds/pwd.c
+++ src/cmds/pwd.c
@@ -6,7 +6,7 @@
 /*   By: cbopp <cbopp@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:03:47 by cbopp             #+#    #+#             */
-/*   Updated: 2025/02/18 17:46:34 by cbopp            ###   ########.fr       */
+/*   Updated: 2025/03/27 22:51:13 by cbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,19 @@
  * @details Uses getcurpath(t_mini *mini) to update mini->cur_path
  * @returns 0 on success, 1 on failure
  */
-int	pwd(t_mini *mini)
+int	pwd(void)
 {
-	getcurpath(mini);
-	if (mini->cur_path != NULL)
+	int		len;
+	char	*buf;
+
+	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	getcwd(buf, BUFFER_SIZE);
+	if (buf != NULL)
 	{
-		write(STDOUT_FILENO, mini->cur_path, ft_strlen(mini->cur_path));
+		len = ft_strlen(buf);
+		write(STDOUT_FILENO, buf, len);
 		write(STDOUT_FILENO, "\n", 1);
+		free(buf);
 		return (0);
 	}
 	perror("pwd");
