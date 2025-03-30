f0265e9f88e9a34a59270a91627d69ac11414161
Hayden <mysterhayde@gmail.com>
2025-03-30 22:51:14 +0200

    reapire multipe error when the file have no permission

--- src/exec/exec_utils.c
+++ src/exec/exec_utils.c
@@ -3,14 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   exec_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbopp <cbopp@student.42lausanne.ch>        +#+  +:+       +#+        */
+/*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 09:35:44 by cbopp             #+#    #+#             */
-/*   Updated: 2025/02/25 18:21:57 by cbopp            ###   ########.fr       */
+/*   Updated: 2025/03/30 22:46:47 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+void	file_access_failed(char *file)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(file, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(strerror(errno), 2);
+	ft_putstr_fd("\n", 2);
+}
 
 void	find_cmd(t_mini *mini)
 {
